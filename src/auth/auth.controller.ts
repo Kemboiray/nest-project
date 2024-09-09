e68919307fc493399ba23f729a4df5fe227426e9
Kemboiray <kemboiray@gmail.com>
2024-09-09 23:45:27 +0300

    Create user threough "/auth/register"

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { CreateUserDto } from "src/users/dto/create-user.dto";
+import { UsersService } from "src/users/users.service";
 
 @Controller("auth")
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post("register")
+  register(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,15 @@
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { User, UserSchema } from "src/users/schemas/user.schema";
+import { UsersService } from "src/users/users.service";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, UsersService],
 })
 export class AuthModule {}
