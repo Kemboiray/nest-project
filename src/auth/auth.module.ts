a2c4faa654abc217294309d477c25e5e66eedb14
Kemboiray <kemboiray@gmail.com>
2024-09-15 13:24:29 +0300

    Replace absolute import path with relative

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,7 +4,7 @@ import { AuthService } from "./auth.service";
 import { JwtModule } from "@nestjs/jwt";
 import { ConfigService } from "@nestjs/config";
 import { AuthGuard } from "./auth.guard";
-import { UsersModule } from "src/users/users.module";
+import { UsersModule } from "../users/users.module";
 
 @Module({
   imports: [
