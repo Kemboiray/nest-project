cad2e2dc8eeb4b879ce3bbccd8af66193874df55
Kemboiray <kemboiray@gmail.com>
2024-09-15 12:44:28 +0300

    Remove unused import

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,4 @@
-import {
-  Injectable,
-  NotFoundException,
-  UnauthorizedException,
-} from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { UsersService } from "../users/users.service";
 import * as bcrypt from "bcrypt";
 import { LogInDto } from "./dto/login.dto";
